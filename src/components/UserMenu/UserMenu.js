d94ac01b61caa13d01fe372bb92edd0bafd92dd1
RomaYourchuk <yourchuk922@gmail.com>
2022-07-28 00:22:46 +0300

    added email to the menu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -15,17 +15,22 @@ const styles = {
     fontWeight: 700,
     marginRight: 12,
   },
+  link: {
+    marginRight: 12,
+  }
 };
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUsername);
   const avatar = defaultAvatar;
+  const email = useSelector(authSelectors.getUseremail);
 
   return (
     <div style={styles.container}>
       <img src={avatar} alt="" width="32" style={styles.avatar} />
       <span style={styles.name}>Welcome, {name}</span>
+      <span style={styles.link}>{email}</span>
       <button type="button" className={s.button} onClick={() => dispatch(authOperations.logOut())}>
         Sign Out
       </button>
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,9 +1,11 @@
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUsername = state => state.auth.user.name;
+const getUseremail = state => state.auth.user.email;
 
 const authSelectors = {
   getIsLoggedIn,
   getUsername,
+  getUseremail
 };
 export default authSelectors;
